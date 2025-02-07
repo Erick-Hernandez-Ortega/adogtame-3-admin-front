14ed91c9849686801818b30cc1e10bb9a4389017
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-06 20:39:33 -0600

    feat: cambiar informacion de login form

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/shared/components/ui/button";
 import { Input } from "@/shared/components/ui/input";
 import { Label } from "@/shared/components/ui/label";
 import { cn } from "@/shared/lib/utils";
+import Link from "next/link";
 
 export function LoginForm({
   className,
@@ -10,37 +11,31 @@ export function LoginForm({
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Adogtame</h1>
+        <h1 className="text-2xl font-bold">Adogtame administrador</h1>
         <p className="text-balance text-sm text-muted-foreground">
-          Enter your email below to login to your account
+          Inicia sesión
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
+          <Label htmlFor="email">Correo electrónico</Label>
           <Input id="email" type="email" placeholder="m@example.com" required />
         </div>
         <div className="grid gap-2">
           <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-            <a
+            <Label htmlFor="password">Contraseña</Label>
+            <Link
               href="#"
               className="ml-auto text-sm underline-offset-4 hover:underline"
             >
-              Forgot your password?
-            </a>
+              Olvidaste tu contraseña?
+            </Link>
           </div>
           <Input id="password" type="password" required />
         </div>
         <Button type="submit" className="w-full">
           Login
         </Button>
-      </div>
-      <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <a href="#" className="underline underline-offset-4">
-          Sign up
-        </a>
       </div>
     </form>
   )
