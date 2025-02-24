1c17c106bc2712a845248aee7f27e83d12e8d7ca
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-23 22:19:19 -0600

    feat: crear validacion en el middleware de token

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,16 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/auth', request.url));
-  }
+  const token: string | undefined = request.cookies.get("token")?.value;
+  const isLoginPage: boolean = request.nextUrl.pathname === "/auth";
+
+  if (!token && !isLoginPage) return NextResponse.redirect(new URL("/auth", request.url));
+  
+  if (token && isLoginPage) return NextResponse.redirect(new URL("/dashboard", request.url));
+  
+  return NextResponse.next(); 
 }
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api/).*)"],
+};
