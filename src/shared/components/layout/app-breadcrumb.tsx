65dc88fce3d2866fc9cfb1d32a2f0aca033a874e
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-02-24 19:53:36 -0600

    feat: cambiar en app breadcrumb

--- src/shared/components/layout/app-breadcrumb.tsx
+++ src/shared/components/layout/app-breadcrumb.tsx
@@ -2,28 +2,36 @@
 import { JSX } from "react";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/shared/components/ui/breadcrumb";
 import { usePathname } from "next/navigation";
+import { links } from "@/shared/data/nav-links";
 
 export const AppBreadcrumb = (): JSX.Element => {
     const pathname: string = usePathname();
-    const pathSegments: string[] = pathname.split("/").filter(Boolean)
+    const pathSegments: string[] = pathname.split("/").filter(Boolean);
+    const mainCategory = links.find((category) =>
+        category.items.some((item) => pathname.startsWith(item.url))
+    );
 
     return (
         <Breadcrumb>
             <BreadcrumbList>
-                <BreadcrumbItem>
-                    <BreadcrumbLink href="/">Inicio</BreadcrumbLink>
-                </BreadcrumbItem>
+                {mainCategory && (
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href={mainCategory?.items[0]?.url}>
+                            {mainCategory?.title}
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                )}
 
                 {pathSegments.map((segment, index) => {
                     const href: string = `/${pathSegments.slice(0, index + 1).join("/")}`;
-                    const isLast: boolean = index === pathSegments.length - 1;
+                    const isLast: boolean = index === pathSegments?.length - 1;
 
                     return (
                         <div key={href} className="flex items-center">
                             <BreadcrumbSeparator />
                             <BreadcrumbItem>
                                 {isLast ? (
-                                    <BreadcrumbPage>{decodeURIComponent(segment)}</BreadcrumbPage>
+                                    <BreadcrumbPage className="capitalize">{decodeURIComponent(segment)}</BreadcrumbPage>
                                 ) : (
                                     <BreadcrumbLink href={href}>
                                         {decodeURIComponent(segment)}
