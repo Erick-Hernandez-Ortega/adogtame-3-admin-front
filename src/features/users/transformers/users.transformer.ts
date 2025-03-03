48386eae013d20d62a34b676fb9c8d239182ece6
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-02 20:39:53 -0600

    feat(users): crear transformer

---
+++ src/features/users/transformers/users.transformer.ts
@@ -0,0 +1,24 @@
+interface UserResponse {
+    _id: string;
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    age: string;
+    imagen: string;
+    isTokenRemoved: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export const usersTransformer = (user: UserResponse) => {
+    return {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        username: user.username,
+        age: user.age,
+        image: user.imagen,
+        createdAt: user.createdAt
+    }
+}
