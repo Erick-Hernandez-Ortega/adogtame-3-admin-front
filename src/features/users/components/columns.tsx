9f007d585087f67b893bb0ae1394f7af319c36e6
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-02 20:13:20 -0600

    feat(users): crear columns

---
+++ src/features/users/components/columns.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { Button } from "@/shared/components/ui/button"
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
+
+export type PersonColumn = {
+    // id: string
+    // amount: number
+    // status: "pending" | "processing" | "success" | "failed"
+    // email: string
+}
+
+export const columns: ColumnDef<PersonColumn>[] = [
+    {
+        accessorKey: "_id",
+        header: "ID",
+    },
+    {
+        accessorKey: "name",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    className="p-0"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Nombre
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "username",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    className="p-0"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Nombre de Usuario
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "email",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    className="p-0"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Email
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "age",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    className="p-0"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Edad
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Fecha de Creación",
+    },
+]
