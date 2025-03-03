732ce5c504fe22acc8d74f7a354ea7feafdf9299
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-02 20:48:49 -0600

    feat(users): crear tabla

--- src/app/(admin)/usuarios/page.tsx
+++ src/app/(admin)/usuarios/page.tsx
@@ -1,9 +1,15 @@
+import { getUsers } from "@/features/users/actions/get-users";
+import { columns } from "@/features/users/components/columns";
+import { DataTable } from "@/features/users/components/data-table";
 import HeaderUsers from "@/features/users/components/header-users";
 
-export default function Usuarios() {
+export default async function Usuarios() {
+    const { users } = await getUsers();
+
     return (
         <>
             <HeaderUsers />
+            <DataTable columns={columns} data={users} />
         </>
     );
 }
