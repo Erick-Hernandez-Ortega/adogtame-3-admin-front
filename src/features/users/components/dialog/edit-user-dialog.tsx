17b185b6c471d7ef245cdfbe990581494737f543
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-10 20:55:11 -0600

    feat(users): crear modal de editar

--- src/features/users/components/dialog/edit-user-dialog.tsx
+++ src/features/users/components/dialog/edit-user-dialog.tsx
@@ -1,20 +1,22 @@
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/shared/components/ui/dialog"
 import { useModalStore } from "@/shared/store/modal-store";
 import { JSX } from "react";
+import { EditUserForm } from "../forms/edit-user-form";
 
 export const EditUserDialog = (): JSX.Element => {
     const { closeEditModalUser } = useModalStore();
 
     return (
         <Dialog open onOpenChange={closeEditModalUser}>
-            <DialogContent >
+            <DialogContent className="sm:max-w-[525px]">
                 <DialogHeader>
                     <DialogTitle>Editar usuario</DialogTitle>
                     <DialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
+                        Edita el usuario de la plataforma
                     </DialogDescription>
                 </DialogHeader>
+
+                <EditUserForm />
             </DialogContent>
         </Dialog>
     )
