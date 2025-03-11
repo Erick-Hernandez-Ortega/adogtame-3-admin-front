57352652654d3f0a5bf32d606211f13a004e9974
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-10 20:24:54 -0600

    feat: crear componente column

--- src/features/users/components/columns.tsx
+++ src/features/users/components/columns.tsx
@@ -1,11 +1,45 @@
 "use client"
 
 import { Button } from "@/shared/components/ui/button"
-import { ColumnDef } from "@tanstack/react-table"
+import { ColumnDef, Row } from "@tanstack/react-table"
 import { ArrowUpDown, Copy, Pencil, Trash } from "lucide-react"
 import { User } from "../types/user.types"
 import { MoreHorizontal } from "lucide-react"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/shared/components/ui/dropdown-menu"
+import { useModalStore } from "@/shared/store/modal-store";
+
+const ActionCell = ({ row }: { row: Row<User> }) => {
+    const user = row.original;
+    const { openEditModalUser } = useModalStore();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+                <DropdownMenuItem
+                    onClick={() => navigator.clipboard.writeText(user.id)}
+                >
+                    <Copy className="h-4 w-4" />
+                    Copiar ID
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => openEditModalUser(user)}>
+                    <Pencil className="h-4 w-4" />
+                    Editar
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Trash className="h-4 w-4" />
+                    Eliminar
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
 
 export const columns: ColumnDef<User>[] = [
     {
@@ -78,37 +112,6 @@ export const columns: ColumnDef<User>[] = [
     },
     {
         id: "actions",
-        cell: ({ row }) => {
-            const user = row.original
-
-            return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Abrir Menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Acciones</DropdownMenuLabel>
-                        <DropdownMenuItem
-                            onClick={() => navigator.clipboard.writeText(user.id)}
-                        >
-                            <Copy className="h-4 w-4" />
-                            Copiar ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                            <Pencil className="h-4 w-4" />
-                            Editar
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <Trash className="h-4 w-4" />
-                            Eliminar
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            )
-        },
+        cell: ({ row }) => <ActionCell row={row} />
     },
 ]
