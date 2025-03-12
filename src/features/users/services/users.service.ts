1afdb1298f87d380fa22536083bde57132549b9c
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-11 20:18:29 -0600

    feat(users): crear servicio de actualizar

--- src/features/users/services/users.service.ts
+++ src/features/users/services/users.service.ts
@@ -1,5 +1,5 @@
 import { cookies } from "next/headers";
-import { UserCreate } from "../types/user.types";
+import { UserCreate, UserEdit } from "../types/user.types";
 
 export const createUser = async (user: UserCreate) => {
     "use server";
@@ -12,6 +12,22 @@ export const createUser = async (user: UserCreate) => {
             "Authorization": `Bearer ${token}`,
         },
         method: "POST",
+    });
+
+    return response.json();
+}
+
+export const updateUser = async (user: UserEdit) => {
+    "use server";
+    const token: string | undefined = (await cookies()).get("token")?.value;
+
+    const response: Response = await fetch(`${process.env.API_URL}/users/${user.id}`, {
+        body: JSON.stringify(user),
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+        method: "PATCH",
     });
 
     return response.json();
