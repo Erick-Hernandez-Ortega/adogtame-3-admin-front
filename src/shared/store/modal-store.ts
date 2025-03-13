1574c2252baa7083d6d4de54a6c57df79995489c
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-12 21:17:54 -0600

    feat(users): agregar modal remove en la store

--- src/shared/store/modal-store.ts
+++ src/shared/store/modal-store.ts
@@ -3,14 +3,20 @@ import { create } from 'zustand'
 
 interface ModalState {
     isVisibleModalUserEdit: boolean;
+    isVisibleModalUserRemove: boolean;
     selectedUser: User | null;
     openEditModalUser: (user: User) => void;
+    openRemoveModalUser: (user: User) => void;
     closeEditModalUser: () => void;
+    closeRemoveModalUser: () => void;
 }
 
 export const useModalStore = create<ModalState>((set) => ({
    isVisibleModalUserEdit: false,
+   isVisibleModalUserRemove: false,
    selectedUser: null,
    openEditModalUser: (user: User) => set({ isVisibleModalUserEdit: true, selectedUser: user }),
+   openRemoveModalUser: (user: User) => set({ isVisibleModalUserRemove: true, selectedUser: user }),
    closeEditModalUser: () => set({ isVisibleModalUserEdit: false, selectedUser: null }),
+   closeRemoveModalUser: () => set({ isVisibleModalUserRemove: false, selectedUser: null }),
 }));
