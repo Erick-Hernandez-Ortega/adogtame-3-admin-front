1b95c3d6788ec974b5eb1d4f6e7bab6d5af0cc0a
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-14 17:33:50 -0600

    feat(users): agregar que cierre solo el modal

--- src/features/users/hooks/use-user-remove-action.ts
+++ src/features/users/hooks/use-user-remove-action.ts
@@ -1,12 +1,19 @@
-import { useActionState, useMemo } from "react";
+import { useActionState, useEffect, useMemo } from "react";
 import { removeUser } from "../actions/remove-user";
 import { useModalStore } from "@/shared/store/modal-store";
 
 export const useUserRemoveAction = () => {
     const initialState = useMemo(() => ({ errors: { general: '' } }), []);
     const [state, formAction] = useActionState(removeUser, initialState);
-    const { selectedUser } = useModalStore();
-    
+    const { selectedUser, closeRemoveModalUser } = useModalStore();
+
+    useEffect(() => {
+        if (state !== initialState) {
+            const hasErrors: boolean = Object.values(state.errors || {}).some(error => error);
+            if (!hasErrors) return closeRemoveModalUser();
+        }
+    }, [state, initialState, closeRemoveModalUser]);
+
     return {
         state,
         formAction,
