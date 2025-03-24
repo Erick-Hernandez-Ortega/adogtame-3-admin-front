1e4bd83af5305c10377cd4c9b8f306c9a409c484
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-23 20:45:17 -0600

    feat(pets): agregar requerid

--- src/features/pets/components/radio-group/stirilized-radio.tsx
+++ src/features/pets/components/radio-group/stirilized-radio.tsx
@@ -4,7 +4,7 @@ import { RadioGroup, RadioGroupItem } from "@/shared/components/ui/radio-group";
 export const StirilizedRadio = ({ name }: { name: string }) => {
 
     return (
-        <RadioGroup defaultValue="false" name={name} className="flex flex-row">
+        <RadioGroup defaultValue="false" required name={name} className="flex flex-row">
             <div className="flex items-center space-x-2">
                 <RadioGroupItem value="true" id="Yes" />
                 <Label htmlFor="Yes">Si</Label>
