9c779fe67ccebc5178170585090a1f85d818de13
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-23 21:13:03 -0600

    feat(pets): crear types

---
+++ src/features/pets/types/pet.types.ts
@@ -0,0 +1,10 @@
+export interface PetForm {
+    name: string;
+    breed: string;
+    age: string;
+    description: string;
+    stirilized: boolean;
+    sex: string;
+    typeOfPet: string;
+    size: string;
+}
