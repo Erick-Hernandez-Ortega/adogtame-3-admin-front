7d9e03aa3cebaa0469663aacc4779ea096bfcb7a
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-27 20:13:25 -0600

    feat(pets): agregar nueva interfaz

--- src/features/pets/types/pet.types.ts
+++ src/features/pets/types/pet.types.ts
@@ -9,3 +9,20 @@ export interface PetForm {
     size: string;
     owner: string;
 }
+
+export interface PetResponse {
+    _id: string;
+    name: string;
+    breed: string;
+    age: string;
+    description: string;
+    stirilized: boolean;
+    sex: 'Male' | 'Female' | 'Unknown';
+    typeOfPet: 'Dog' | 'Cat' | 'Other';
+    size: 'Little' | 'Medium' | 'Large' | 'VeryLarge';
+    available: boolean;
+    owner: string;
+    image: string;
+    createdAt: string;
+    updatedAt: string;
+}
