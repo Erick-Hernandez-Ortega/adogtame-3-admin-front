31a6f0c9b3c65bfeca318e4c362e999765e08f9c
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-27 20:32:37 -0600

    feat(pets): agregar data table a la pagina

--- src/app/(admin)/mascotas/page.tsx
+++ src/app/(admin)/mascotas/page.tsx
@@ -1,7 +1,15 @@
+import getPets from "@/features/pets/actions/get-pets";
 import { HeaderPets } from "@/features/pets/components/header-pets";
+import { columnsPets } from "@/features/pets/components/table/columns-pets";
+import { DataTablePets } from "@/features/pets/components/table/data-table-pets";
 
-export default function Mascotas() {
+export default async function Mascotas() {
+    const { pets } = await getPets();
+
     return (
-        <HeaderPets />
+        <>
+            <HeaderPets />
+            <DataTablePets data={pets} columns={columnsPets} />
+        </>
     );
 }
