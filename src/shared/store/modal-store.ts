fbbc8c9cd2c830d955d8998f53500b9b8c661d96
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-03-28 19:00:13 -0600

    feat(pets): agregar pet a modal store

--- src/shared/store/modal-store.ts
+++ src/shared/store/modal-store.ts
@@ -1,7 +1,9 @@
+import { Pet } from '@/features/pets/types/pet.types';
 import { User } from '@/features/users/types/user.types';
-import { create } from 'zustand'
+import { create } from 'zustand';
 
 interface ModalState {
+    // ? User
     isVisibleModalUserEdit: boolean;
     isVisibleModalUserRemove: boolean;
     selectedUser: User | null;
@@ -9,14 +11,27 @@ interface ModalState {
     openRemoveModalUser: (user: User) => void;
     closeEditModalUser: () => void;
     closeRemoveModalUser: () => void;
+
+    // ? Pet
+    isVisibleModalPetRemove: boolean;
+    selectedPet: Pet | null;
+    openModalRemovePet: (pet: Pet) => void;
+    closeModalRemovePet: () => void;
 }
 
 export const useModalStore = create<ModalState>((set) => ({
-   isVisibleModalUserEdit: false,
-   isVisibleModalUserRemove: false,
-   selectedUser: null,
-   openEditModalUser: (user: User) => set({ isVisibleModalUserEdit: true, selectedUser: user }),
-   openRemoveModalUser: (user: User) => set({ isVisibleModalUserRemove: true, selectedUser: user }),
-   closeEditModalUser: () => set({ isVisibleModalUserEdit: false, selectedUser: null }),
-   closeRemoveModalUser: () => set({ isVisibleModalUserRemove: false, selectedUser: null }),
+    // ? User
+    isVisibleModalUserEdit: false,
+    isVisibleModalUserRemove: false,
+    selectedUser: null,
+    openEditModalUser: (user: User) => set({ isVisibleModalUserEdit: true, selectedUser: user }),
+    openRemoveModalUser: (user: User) => set({ isVisibleModalUserRemove: true, selectedUser: user }),
+    closeEditModalUser: () => set({ isVisibleModalUserEdit: false, selectedUser: null }),
+    closeRemoveModalUser: () => set({ isVisibleModalUserRemove: false, selectedUser: null }),
+
+    // ? Pet
+    selectedPet: null,
+    isVisibleModalPetRemove: false,
+    openModalRemovePet: (pet: Pet) => set({ isVisibleModalPetRemove: true, selectedPet: pet }),
+    closeModalRemovePet: () => set({ isVisibleModalPetRemove: false, selectedPet: null }),
 }));
