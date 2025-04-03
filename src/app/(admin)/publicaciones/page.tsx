249bec7eb0e838fae4462f7f309765f811c1f61a
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-02 21:09:59 -0600

    feat(publication): agregar tabla y datos a la pagina

--- src/app/(admin)/publicaciones/page.tsx
+++ src/app/(admin)/publicaciones/page.tsx
@@ -1,9 +1,15 @@
+import getPublications from "@/features/publications/actions/get-publications";
 import { HeaderPublications } from "@/features/publications/components/header-publications";
+import { columnsPublications } from "@/features/publications/components/table/columns-publications";
+import { DataTablePublications } from "@/features/publications/components/table/data-table-publications";
 
 export default async function Publicaciones() {
+    const { publications } = await getPublications();
+
     return (
         <>
             <HeaderPublications />
+            <DataTablePublications columns={columnsPublications} data={publications} />
         </>
     );
 }
