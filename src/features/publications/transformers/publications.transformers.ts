fafe729852f25401f82e73bbad066c154b0f7047
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-02 20:56:57 -0600

    feat(publication): modificar transformer

--- src/features/publications/transformers/publications.transformers.ts
+++ src/features/publications/transformers/publications.transformers.ts
@@ -1,10 +1,11 @@
 import { PublicationResponse } from "../types/publication.types"
 
 export const publicationTransformer = (publication: PublicationResponse) => {
+    console.log(publication, 'publicationeeee');
     return {
         id: publication._id,
-        ownerPublication: publication.ownerPublication,
-        petId: publication.petId,
+        ownerPublication: publication.ownerId,
+        petPublication: publication.petId,
         title: publication.title,
         description: publication.description,
         status: publication.status,
