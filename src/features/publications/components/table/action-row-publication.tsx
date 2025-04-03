627d47051d0e055fa5490132f25145eec0c6611c
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-02 21:13:50 -0600

    feat(publication): crear action row

---
+++ src/features/publications/components/table/action-row-publication.tsx
@@ -0,0 +1,39 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/shared/components/ui/dropdown-menu"
+import { Row } from "@tanstack/react-table";
+import { Button } from "@/shared/components/ui/button";
+import { Copy, MoreHorizontal, Trash } from "lucide-react";
+import { useModalStore } from "@/shared/store/modal-store";
+import { Publication } from "../../types/publication.types";
+
+export const ActionRowPublication = ({ row }: { row: Row<Publication> }) => {
+    const publication: Publication = row.original;
+    const { openModalRemovePublication } = useModalStore();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+                <DropdownMenuItem
+                    onClick={() => navigator.clipboard.writeText(publication.id)}
+                >
+                    <Copy className="h-4 w-4" />
+                    Copiar ID
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                {/* <DropdownMenuItem onClick={() => openModalEditPet(pet)}>
+                    <Pencil className="h-4 w-4" />
+                    Editar
+                </DropdownMenuItem> */}
+                <DropdownMenuItem onClick={() => openModalRemovePublication(publication)}>
+                    <Trash className="h-4 w-4" />
+                    Eliminar
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
