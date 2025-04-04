f151bd5668a00ff81796830accb17c31b26a273d
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-03 21:36:13 -0600

    fix(publication): cerrar modal

--- src/features/publications/hooks/use-remove-publication.ts
+++ src/features/publications/hooks/use-remove-publication.ts
@@ -5,14 +5,14 @@ import { useModalStore } from "@/shared/store/modal-store";
 export const useRemovePublication = () => {
     const initialState = useMemo(() => ({ errors: { general: '' } }), []);
     const [state, formAction] = useActionState(removePublication, initialState);
-    const { selectedPublication, closeModalRemovePet } = useModalStore();
+    const { selectedPublication, closeModalRemovePublication } = useModalStore();
 
     useEffect(() => {
             if (state !== initialState) {
                 const hasErrors: boolean = Object.values(state.errors || {}).some(error => error);
-                if (!hasErrors) return closeModalRemovePet();
+                if (!hasErrors) return closeModalRemovePublication();
             }
-        }, [state, initialState, closeModalRemovePet]);
+        }, [state, initialState, closeModalRemovePublication]);
 
     return {
         state,
