9e5de4c4b23b11c5c10da382fc2c0b92832120b9
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-06 21:31:36 -0600

    feat(adoptions): crear componente action row

---
+++ src/features/adoptions/components/table/action-row-adoption.tsx
@@ -0,0 +1,39 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/shared/components/ui/dropdown-menu"
+import { Row } from "@tanstack/react-table";
+import { Button } from "@/shared/components/ui/button";
+import { Copy, MoreHorizontal } from "lucide-react";
+// import { useModalStore } from "@/shared/store/modal-store";
+import { Adoption } from "../../types/adoptions.types";
+
+export const ActionRowAdoption = ({ row }: { row: Row<Adoption> }) => {
+    const adoption: Adoption = row.original;
+    // const { openModalRemovePet, openModalEditPet } = useModalStore();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+                <DropdownMenuItem
+                    onClick={() => navigator.clipboard.writeText(adoption.id)}
+                >
+                    <Copy className="h-4 w-4" />
+                    Copiar ID
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                {/* <DropdownMenuItem onClick={() => openModalEditPet(pet)}>
+                    <Pencil className="h-4 w-4" />
+                    Editar
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => openModalRemovePet(pet)}>
+                    <Trash className="h-4 w-4" />
+                    Eliminar
+                </DropdownMenuItem> */}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
