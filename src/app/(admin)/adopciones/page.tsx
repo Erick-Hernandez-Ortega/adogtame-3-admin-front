581f97f1c6db825abb6d6b538328f5e8c9af5e71
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-06 21:41:41 -0600

    feat(adoptions): crear componente table

--- src/app/(admin)/adopciones/page.tsx
+++ src/app/(admin)/adopciones/page.tsx
@@ -1,9 +1,15 @@
+import getAdoptions from "@/features/adoptions/actions/get-adoptions";
 import { HeaderAdoptions } from "@/features/adoptions/components/header-adoptions";
+import { columnsAdoptions } from "@/features/adoptions/components/table/columns-adoptions";
+import { DataTableAdoptions } from "@/features/adoptions/components/table/data-table-adoptions";
 
 export default async function Adopciones() {
+    const { adoptions } = await getAdoptions();
+
     return (
         <>
             <HeaderAdoptions />
+            <DataTableAdoptions columns={columnsAdoptions} data={adoptions} />
         </>
     );
 }
