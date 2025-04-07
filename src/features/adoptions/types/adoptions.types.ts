2c377921792dbf84bc51b09f056f0030e3f5e22b
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-06 21:12:56 -0600

    feat(adoptions): crear types

---
+++ src/features/adoptions/types/adoptions.types.ts
@@ -0,0 +1,35 @@
+interface UserDetailsResponse {
+    _id: string;
+    name: string;
+    username: string;
+    age: string;
+    email: string;
+    imagen: string;
+    isTokenRemoved: boolean;
+}
+
+interface PetDetailsResponse {
+    _id: string;
+    name: string;
+    breed: string;
+    age: string;
+    description: string;
+    stirilized: boolean;
+    sex: "Male" | "Female" | "Unknown";
+    typeOfPet: "Dog" | "Cat" | "Other";
+    size: "Little" | "Medium" | "Large" | "VeryLarge";
+    available: boolean;
+    owner: string
+  }
+
+
+export  interface AdoptionResponse {
+    _id: string,
+    petId: PetDetailsResponse;
+    userId: UserDetailsResponse;
+    status: "pending" | "approved" | "rejected" | "completed";
+    reason: string;
+    comments: string;
+    createdAt: string;
+    updatedAt: string;
+}
