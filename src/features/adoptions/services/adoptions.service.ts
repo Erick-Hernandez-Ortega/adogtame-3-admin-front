afa49f643838122f738152c52b51397d4aab93ce
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-10 22:15:11 -0600

    feat(adoption): crear service

--- src/features/adoptions/services/adoptions.service.ts
+++ src/features/adoptions/services/adoptions.service.ts
@@ -28,3 +28,19 @@ export const deleteAdoption = async (id: string): Promise<unknown> => {
 
     return response.json();
 }
+
+export const updateAdoption = async (id: string, adoption: { comments: string, reason: string, status: string }): Promise<unknown> => {
+    "use server";
+    const token: string | undefined = (await cookies()).get("token")?.value;
+
+    const response: Response = await fetch(`${process.env.API_URL}/adopcions/${id}`, {
+        body: JSON.stringify(adoption),
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+        method: "PATCH",
+    });
+
+    return response.json();
+}
