d80a0cdf661ac47a5d9434af1f6ff628a47803fd
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-10 21:53:24 -0600

    feat(adoption): crear hook de editar

---
+++ src/features/adoptions/hooks/use-adoption-update.ts
@@ -0,0 +1,23 @@
+import { useModalStore } from "@/shared/store/modal-store";
+import { useActionState, useEffect, useMemo } from "react";
+import editAdoption from "../actions/edit-adoption";
+
+export const useAdoptionUpdate = () => {
+    const initialState = useMemo(() => ({ errors: { general: '', comments: '', reason: '' } }), []);
+    const [state, formAction] = useActionState(editAdoption, initialState);
+    const { selectedAdoption, closeModalEditAdoption } = useModalStore();
+
+
+    useEffect(() => {
+        if (state !== initialState) {
+            const hasErrors: boolean = Object.values(state.errors || {}).some(error => error);
+            if (!hasErrors) return closeModalEditAdoption();
+        }
+    }, [state, initialState, closeModalEditAdoption]);
+
+    return {
+        state,
+        formAction,
+        selectedAdoption
+    }
+}
