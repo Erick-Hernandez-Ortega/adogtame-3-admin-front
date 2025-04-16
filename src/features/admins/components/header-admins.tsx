b44329bb38a0ab48e1f03322d6dedc714cf62d4f
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-15 20:18:01 -0600

    feat(admins): crear header

---
+++ src/features/admins/components/header-admins.tsx
@@ -0,0 +1,12 @@
+export const HeaderAdmins = () => {
+    return (
+        <header className="flex items-center justify-between">
+            <div>
+                <h2 className="text-2xl mb-2">Adminstradores</h2>
+                <p className="tracking-wide text-sm leading-relaxed">
+                    Visualiza de forma eficiente a los administradores de tu plataforma.
+                </p>
+            </div>
+        </header>
+    );
+}
