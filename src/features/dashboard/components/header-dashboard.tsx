c9c47bbde294c42ab552b08f546a83e2bff2ba27
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-21 21:47:11 -0600

    feat(dashboard): crear header

---
+++ src/features/dashboard/components/header-dashboard.tsx
@@ -0,0 +1,14 @@
+export const HeaderDashboard = () => {
+    return (
+        <header className="flex items-center justify-between">
+            <div>
+                <h2 className="text-2xl mb-2">Dashboard</h2>
+                <p className="tracking-wide text-sm leading-relaxed">
+                    Visualiza y gestiona de manera centralizada todos los aspectos de tu plataforma.
+                    Este dashboard te ofrece una visión general intuitiva de los usuarios, las adopciones, las publicaciones y otras métricas clave.
+                    Administra perfiles de usuario, supervisa el flujo de adopciones, controla el contenido publicado y obtén información valiosa para optimizar la experiencia de tu comunidad.
+                </p>
+            </div>
+        </header>
+    );
+}
