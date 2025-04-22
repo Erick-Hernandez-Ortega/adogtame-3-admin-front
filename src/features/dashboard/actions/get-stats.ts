5c0c664042aa849f321e08fc7230767da2e359e0
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-21 20:22:33 -0600

    feat(dashboard): obtener users de action

--- src/features/dashboard/actions/get-stats.ts
+++ src/features/dashboard/actions/get-stats.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { findAllAdoptions, findAllAvailablePets, findAllPublications } from "../services/dashboard.service";
+import { findAllAdoptions, findAllAvailablePets, findAllPublications, findAllUsers } from "../services/dashboard.service";
 import { dashboardDataTransformer } from "../transformer/dashboard-data.transformer";
 import { Stat } from "../types/dashboard.types";
 
@@ -8,10 +8,12 @@ export default async function getStats() {
     const totalPets: Stat[] = await findAllAvailablePets();
     const totalAdoptions: Stat[] = await findAllAdoptions();
     const totalPublications: Stat[] = await findAllPublications();
+    const totalUsers: Stat = await findAllUsers();
 
     return {
         totalPets: totalPets.map(stat => dashboardDataTransformer(stat)),
         totalAdoptions: totalAdoptions.map(stat => dashboardDataTransformer(stat)),
         totalPublications: totalPublications.map(stat => dashboardDataTransformer(stat)),
+        totalUsers
     }
 }
