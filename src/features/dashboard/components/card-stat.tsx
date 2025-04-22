57e329787138e861857a32657c3528f4c90c091c
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-04-21 20:54:52 -0600

    feat(dashboard): configurar plural en component

--- src/features/dashboard/components/card-stat.tsx
+++ src/features/dashboard/components/card-stat.tsx
@@ -2,18 +2,19 @@ import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/shar
 import { Stat } from "../types/dashboard.types";
 
 export const CardStat = ({ data }: { data: Stat }) => {
+    const isNoun: boolean = data.count === 1;
 
     return (
         <Card className="w-full mx-2">
             <CardHeader className="relative">
                 <CardDescription>{data.label}</CardDescription>
                 <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
-                    {data.count} registros
+                    {data.count} {isNoun ? "registro" : "registros"}
                 </CardTitle>
             </CardHeader>
             <CardFooter className="flex-col items-start gap-1 text-sm">
                 <div className="text-muted-foreground">
-                    Totales de la plataforma
+                    {isNoun ? "un registro" : "registros"} de la plataforma
                 </div>
             </CardFooter>
         </Card>
